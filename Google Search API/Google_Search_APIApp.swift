e65beb714743f7974630a04cb967321d698d547e
Darren Darlington <darrend42@outlook.com>
2021-03-05 10:41:47 -0600

    Initial Commit

---
+++ Google Search API/Google_Search_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_Search_APIApp.swift
+//  Google Search API
+//
+//  Created by Darren Darlington on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Google_Search_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
