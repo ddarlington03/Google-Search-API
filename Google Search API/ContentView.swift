989945c1cbca8e822e119a89e497d90ce07f9efd
Darren Darlington <darrend42@outlook.com>
2021-03-11 10:59:28 -0600

    Finished google search app

--- Google Search API/ContentView.swift
+++ Google Search API/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var entries = [Entry]()
+    @State private var showingAlert = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(entries) { entry in
+                Link(destination: URL(string: entry.link)!, label: {
+                    Text(entry.title)
+                })
+            }
+            .navigationTitle("Elon Musk")
+        }
+        .onAppear(perform: {
+            queryAPI()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("Ok")))
+        })
+    }
+    
+    
+    func queryAPI() {
+        let apiKey = "?rapidapi-key=32a3795ffdmshbeee571acffa848p1c5fecjsn7024003cbda5"
+        let query = "https://google-search3.p.rapidapi.com/api/v1/search/q=elon+musk&num=100\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json["results"].arrayValue
+                for item in contents {
+                    let title = item["title"].stringValue
+                    let link = item["link"].stringValue
+                    let entry = Entry(title: title, link: link)
+                    entries.append(entry)
+                }
+                return
+            }
+        }
+        showingAlert = true
     }
 }
 
@@ -18,4 +54,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Entry: Identifiable {
+    let id = UUID()
+    let title: String
+    let link: String
 }
